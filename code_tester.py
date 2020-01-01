428803a651f77f0e04745252cdb407b8abdb301e
shane-liu-1010 <59184894+shane-liu-1010@users.noreply.github.com>
2020-01-01 21:40:18 +0800

    Update code_tester.py

--- code_tester.py
+++ code_tester.py
@@ -75,6 +75,10 @@ def go():
     else :
         tk.messagebox.showinfo(message="尚未有該程式語言")
 
+def tab(arg):
+    arg.widget.insert(tk.INSERT, " " * 4)
+    return 'break'
+
 window = tk.Tk()
 window.geometry('1000x600')
 window.configure(background='white')
@@ -83,14 +87,14 @@ window.title("Code Tester")
 left_frame = tk.Frame(window)
 left_frame.pack(side=tk.LEFT)
 
-left_lebel = tk.Label(left_frame, text='code:')
+left_lebel = tk.Label(left_frame, font=("Consolas",10), text='code:')
 left_lebel.pack()
 
-left_combo = tt.Combobox(left_frame,width=50,value=['C++17','C++','Python2','Python3'])
+left_combo = tt.Combobox(left_frame,width=40,value=['C++17','C++','Python2','Python3'])
 left_combo.current(0)
 left_combo.pack()
 
-left_text = tk.Text(left_frame,width=70,height=40,bg='aliceblue')
+left_text = tk.Text(left_frame,width=50,height=40,font=("Consolas",15),bg='aliceblue')
 left_text.pack(padx=30,pady=10)
 
 right_frame = tk.Frame(window)
@@ -102,10 +106,10 @@ input_frame.pack()
 in_lebel = tk.Label(input_frame, text=' input:',width=8)
 in_lebel.pack(side=tk.LEFT)
 
-input = tk.Text(input_frame,width=50,height=18,bg='aliceblue')
+input = tk.Text(input_frame,width=30,height=10,font=("Consolas",15),bg='aliceblue')
 input.pack(side=tk.LEFT)
 
-bottom_button = tk.Button(right_frame, text='GO!',font=("Concolas",20),command=go,width=10)
+bottom_button = tk.Button(right_frame, text='GO!',font=("Consolas",20),command=go,width=10)
 bottom_button.pack(pady=20)
 
 output_frame = tk.Frame(right_frame)
@@ -114,7 +118,10 @@ output_frame.pack(pady=5)
 out_lebel = tk.Label(output_frame, text='output:',width=8)
 out_lebel.pack(side=tk.LEFT)
 
-output = tk.Text(output_frame,width=50,height=18,bg='aliceblue')
+output = tk.Text(output_frame,width=50,height=10,font=("Consolas",15),bg='aliceblue')
 output.pack(side=tk.LEFT)
 
+left_text.bind("<Tab>",tab)
+input.bind("<Tab>",tab)
+output.bind("<Tab>",tab)
 window.mainloop()
